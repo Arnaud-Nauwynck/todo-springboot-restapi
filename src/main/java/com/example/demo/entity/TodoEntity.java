a57c41cb1e074adf76f88224e1e8d5ef91c3a3a3
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2025-12-16 14:19:31 +0100

    ..

--- src/main/java/com/example/demo/entity/TodoEntity.java
+++ src/main/java/com/example/demo/entity/TodoEntity.java
@@ -3,9 +3,11 @@ package com.example.demo.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Table(name = "todo")
 @Data
 public class TodoEntity {
 
