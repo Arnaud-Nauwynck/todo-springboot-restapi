4499ea0737f7511d45f63389bc186dcb2b900a18
Arnaud Nauwynck <arnaud.nauwynck@gmail.com>
2025-12-16 13:56:20 +0100

    ..

--- src/main/java/com/example/demo/TodoRestController.java
+++ src/main/java/com/example/demo/TodoRestController.java
@@ -1,25 +1,40 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/todo")
 public class TodoRestController {
 
+    public static class Todo {
+        public int id;
+        public String title;
+        public String description;
+        public int priority;
+
+        public Todo() {
+        }
+
+        public Todo(int id, String title, String description, int priority) {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+            this.priority = priority;
+        }
+    }
+
     private int idGenerator = 1;
     private int newId() {
         return idGenerator++;
     }
+
     protected List<Todo> todos = new ArrayList<>(List.of(
             new Todo(newId(), "Learn Java", "", 1),
             new Todo(newId(), "Learn SpringBoot", "", 2),
